25f13b5ee85eb14c3b4e580a1d38eb3723e389bb
Dirk <rohloff@mbi-berlin.de>
2018-11-09 10:23:22 +0100

    add_new_device

---
+++ add_new_device.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+# add a Phytron-MCC TangoDevice
+
+from PyTango import Database, DbDevInfo, DeviceProxy
+import sys
+import re
+import time
+
+
+NAME = "hhg/MCC2/Ctrl"
+
+ctrl, addr, axis = '','',''
+device_name=''
+
+
+def usage():
+    print("\nusage: add_new_device /hhg/MCC2/Ctrl[nof]_[addr].[motor]")
+    print("example: add_new_device /hhg/MCC2/Ctrl1_0.0")
+    print("   add a device with controller:1, address 0 and axis 0\n")
+    print("   - nof: number of controller")
+    print("   - addr: address of module (0..15)")
+    print("   - motor: motor axis on modul (0/1)")
+    sys.exit(255)
+
+
+def create_device(ctrl, addr, axis):
+    db = Database()
+    new_device_info_mcc = DbDevInfo()
+    new_device_info_mcc._class = "PhytronMcc2"
+    new_device_info_mcc.server = "PhytronMcc2/hhg"
+    
+    new_device = NAME + ctrl + '_' + addr + '.' + axis
+    
+    print("Creating device: %s" %new_device)    
+    new_device_info_mcc.name = new_device
+    db.add_device(new_device_info_mcc)
+    return (new_device)
+    
+
+def create_properties(device_name, addr, axis):
+    CTRL_DEVICE = "hhg/SerialDS/1"
+    property_names = ['Address','Motor','CtrlDevice']
+   
+    mcc = DeviceProxy(device_name)
+    
+    axis_properies = mcc.get_property(property_names)
+    
+    prop_dict =  mcc.get_property(property_names)
+    prop_dict["CtrlDevice"] = CTRL_DEVICE
+    prop_dict['Motor']      = int(axis)
+    prop_dict['Address']    = int(addr)
+    
+    mcc.put_property(prop_dict)
+    
+    
+    
+def check_input():
+    
+    if not(NAME in sys.argv[1]):
+        usage()
+    tmp  = re.split('[.l_]',sys.argv[1])
+    axis = tmp[-1]
+    if not axis.isdigit():
+        print ("\naxis not an integer!")
+        usage()
+    addr = tmp[-2]
+    if not addr.isdigit():
+        print ("\naddr not an integer!")
+        usage()    
+    ctrl = tmp[-3]
+    if not ctrl.isdigit():
+        print ("\nctrl not an integer!")
+        usage()  
+    return (ctrl, addr, axis) 
+
+def main():
+    if len(sys.argv) < 2:
+        usage()
+        
+    ctrl, addr, axis = check_input()
+    device_name= create_device(ctrl, addr, axis)
+    create_properties(device_name, addr, axis)
+
+if __name__ == "__main__":
+    main()    
