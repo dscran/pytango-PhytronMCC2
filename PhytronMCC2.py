8b72b7662ac3db3b742b2a241170040214a23ae2
Daniel Schick <schick@mbi-berlin.de>
2019-12-17 16:05:31 +0100

    updates

--- PhytronMCC2.py
+++ PhytronMCC2.py
@@ -1,7 +1,26 @@
-# PhytronMcc2Run.py
-
+#!/usr/bin/python -u
 from PyTango.server import run
+from PyTango import Database
+import os
 from PhytronMCC2Axis import PhytronMCC2Axis
 from PhytronMCC2Ctrl import PhytronMCC2Ctrl
 
-run([PhytronMCC2Ctrl, PhytronMCC2Axis])
+TANGO_HOST = os.environ['TANGO_HOST']
+print('Check TANGO_HOST: {:s}'.format(TANGO_HOST))
+
+db = Database()
+if db.check_tango_host(TANGO_HOST) == None:
+    print('TANGO_HOST format is okay')
+else:
+    print('TANGO_HOST format is bad')
+
+
+print('Check connection to TANGO database')
+try:
+    print(db.get_info())
+except e as Exception:
+    print('Could not connect to TANGO database')
+    exit(255)
+
+print('Run PhytronMCC2Ctrl & PhytronMCC2Axis')
+run((PhytronMCC2Ctrl, PhytronMCC2Axis,))
