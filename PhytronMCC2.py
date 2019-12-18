9febc325772d9ccf64298af1cb96d37a1d572992
Daniel Schick <schick@mbi-berlin.de>
2019-12-18 17:31:25 +0100

    minimized startup checks

--- PhytronMCC2.py
+++ PhytronMCC2.py
@@ -6,17 +6,11 @@ from PhytronMCC2Axis import PhytronMCC2Axis
 from PhytronMCC2Ctrl import PhytronMCC2Ctrl
 
 TANGO_HOST = os.environ['TANGO_HOST']
-print('Check TANGO_HOST: {:s}'.format(TANGO_HOST))
+print('Trying to connect to TANGO_HOST: {:s}'.format(TANGO_HOST))
 
-db = Database()
-if db.check_tango_host(TANGO_HOST) is None:
-    print('TANGO_HOST format is okay')
-else:
-    print('TANGO_HOST format is bad')
-
-print('Check connection to TANGO database')
 try:
-    print(db.get_info())
+    db = Database()
+    print('Connection established!')
 except Exception:
     print('Could not connect to TANGO database')
     exit(255)
