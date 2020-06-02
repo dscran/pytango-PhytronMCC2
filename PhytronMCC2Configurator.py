90f693dbff5c435d0b8b8ab52daf1db64778c663
Daniel Schick <schick@mbi-berlin.de>
2020-06-02 15:21:33 +0200

    add parameter description to configurator

--- PhytronMCC2Configurator.py
+++ PhytronMCC2Configurator.py
@@ -1,43 +1,43 @@
 from tango import DeviceProxy
 
-class PhytronMCC2Configurator():
+class PhytronMMC2Configurator():
     
     default_config = {
-        1: 0,
-        2: 1,
-        3: 1,
-        4: 400,
-        7: 100000,
-        8: 4000,
-        9: 4000,
-        10: 400,
-        11: 0,
-        12: 0,
-        13: 20,
-        14: 4000,
-        15: 4000,
-        16: 20,
-        17: 0,
-        19: 0,
-        20: 0,
-        21: 0,
-        22: 0,
-        23: 0,
-        24: 0,
-        25: 0,
-        27: 0,
-        34: 0,
-        35: 10,
-        36: 0,
-        38: 0,
-        39: 0,
-        40: 2,
-        41: 6,
-        42: 10,
-        43: 20,
-        45: 4,
-        46: 1,
-        47: 1,
+        1: [0, 'Type of movement'],
+        2: [1, 'Measuring units of movement'],
+        3: [1, 'Conversion factor for the thread'],
+        4: [400, 'Start/stop frequency'],
+        7: [100000, 'Emergency stop ramp'],
+        8: [4000, 'fmaxMØP (mechanical zero point) '],
+        9: [4000, 'Ramp MØP'],
+        10: [400, 'fminMØP Run frequency for leaving the limit switch range'],
+        11: [0, 'MØP offset for limit switch direction +'],
+        12: [0, 'MØP offset for limit switch direction –'],
+        13: [20, 'Recovery time MØP'],
+        14: [4000, 'fmaxRun frequency during program operation'],
+        15: [4000, 'Ramp for run frequency (P14'],
+        16: [20, 'Recovery time position'],
+        17: [0, 'Boost (defined in P42)'],
+        19: [0, 'Electronical zero counter'],
+        20: [0, 'Mechanical zero counter'],
+        21: [0, 'Absolute counter'],
+        22: [0, 'Encoder counter'],
+        23: [0, 'Axial limitation  pos. direction +'],
+        24: [0, 'Axial limitation  neg. direction -'],
+        25: [0, 'Compensation for play'],
+        27: [0, 'Initiator type'],
+        34: [0, 'Encoder type'],
+        35: [10, 'Encoder resolution for SSI encode'],
+        36: [0, 'Encoder function'],
+        38: [0, 'Encoder preferential direction of rotation'],
+        39: [0, 'Encoder conversion factor'],
+        40: [2, 'Stop current'],
+        41: [6, 'Run current'],
+        42: [10, 'Boost current'],
+        43: [20, 'Current delay time in msec'],
+        45: [4, 'Step resolution 1 to 256'],
+        46: [1, 'Current Shaping (CS)'],
+        47: [1, 'Chopper frequency'],
     }
     
     def __init__(self, device):
@@ -47,7 +47,7 @@ class PhytronMCC2Configurator():
     def read_current_config(self):
         self.current_config = {}
         import io
-        raw_conf = d.dump_config()
+        raw_conf = self.proxy.dump_config()
         buf = io.StringIO(raw_conf)
         for line in buf:
             [param, value] = line.strip().split(':')
@@ -56,14 +56,15 @@ class PhytronMCC2Configurator():
     def compare_configs(self, read_current_config=True):
         if read_current_config:
             self.read_current_config()
-        print('Param. \t default \t current \t compare')
-        print('===================================================')
-        for param, value in self.default_config.items():
+        print('Param.\tdefault\t  current\tcompare\tdescription')
+        print('======================================================================')
+        for param, val_list in self.default_config.items():
             curr_value = self.current_config[param]
-            print('P{:02d}: {:10.2f} \t {:10.2f} \t {:s}'.format(param,
-                                                                 value,
+            print('P{:02d}: {:10.2f}\t{:10.2f}\t{:s}\t{:s}'.format(param,
+                                                                 val_list[0],
                                                                  curr_value,
-                                                                 'same' if curr_value == value else 'changed'))
+                                                                 'same' if curr_value == val_list[0] else 'changed',
+                                                                val_list[1],))
             
     def reset_to_default(self):
         for param, value in self.default_config.items():
