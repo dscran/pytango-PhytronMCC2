5c4311f4f7f6b0f8f8f5976e605c3c719c4066ef
Daniel Schick <schick@mbi-berlin.de>
2020-11-16 15:22:23 +0100

    remove print in startup

--- PhytronMCC2.py
+++ PhytronMCC2.py
@@ -4,5 +4,5 @@ import os
 from PhytronMCC2Axis import PhytronMCC2Axis
 from PhytronMCC2Ctrl import PhytronMCC2Ctrl
 
-print('Run PhytronMCC2Ctrl & PhytronMCC2Axis')
+# Run PhytronMCC2Ctrl and PhytronMCC2Axis
 run([PhytronMCC2Ctrl, PhytronMCC2Axis])
